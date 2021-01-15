000d0f12dd40bc24d561aa77881af39edfabb033
ogioko <443484155@qq.com>
2021-01-15 16:21:14 +0800

    创建一个好人 版本3

--- src/main/java/helloWorld.java
+++ src/main/java/helloWorld.java
@@ -10,5 +10,6 @@ public class helloWorld {
         System.out.println("ge");
         System.out.println("aaaa");
         System.out.println("aaaa");
+        System.out.println("cccccccc");
     }
 }
