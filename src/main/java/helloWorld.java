d5710a373078a6bf666b995ea8de0607c6640fb2
ogioko <443484155@qq.com>
2021-01-15 16:12:19 +0800

    创建一个好人

---
+++ src/main/java/helloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * @auther levithanlee
+ * @ccreate--
+ */
+public class helloWorld {
+    public static void main(String[] args) {
+        System.out.println("ni");
+        System.out.println("zhen");
+        System.out.println("shi");
+        System.out.println("ge");
+        System.out.println("aaaa");
+    }
+}
